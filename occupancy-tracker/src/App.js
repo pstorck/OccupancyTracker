dc81334294e071f164e7e62f5c38571682da25ad
Parker Storck <pstorck6@gmail.com>
2021-07-22 15:22:48 -0500

    expandable building cards with progress bars and info

--- occupancy-tracker/src/App.js
+++ occupancy-tracker/src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import React, { Component } from 'react';
+import React from 'react';
+import Collapsible from 'react-collapsible';
+import ProgressBar from "@ramonak/react-progress-bar";
 import './App.css';
 
-function App() {
-  const [data, setData] = React.useState(null);
+function updateProgressColor(occupants) {
+  if (occupants < 50) return 'green';
+  if (occupants < 75) return '#CCCC00';
+  else return 'red';
+}
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
-
+const App = () => {
+  const buildings = [{name: "Rec Center"}, {name: "Admin Buiding"}, {name: "Dining Hall"}]
+  buildings.forEach(building => {building.occupants = Math.floor(Math.random() * 100);})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+    <div id="list-body">
+    <ul>
+      {
+      buildings.map(building => (
+        <li class="building-info">
+            <Collapsible trigger={building.name} triggerStyle={{backgroundColor: "#007AA0", color: 'white'}}>  
+            <ProgressBar bgColor={updateProgressColor(building.occupants)} class="progress-bar" completed={building.occupants} />
+            <p>This is a building. You can do these activities here. Enjoy your time at the builidng!</p>
+            <ul>
+              <li>Address:</li>
+              <li>Hours:</li>
+              <li>Amenities:</li>
+              <li>Capacity:</li>
+            </ul>
+          </Collapsible>
+        </li>
+      ))
+      }
+    </ul>
     </div>
   );
-}
+};
 
 export default App;
